65a9244810482a63baeb69ad1a7e5c69749465ef
mattheworris <mattheworris@me.com>
2017-04-13 14:29:05 +0000

    Working Draft: Everything seems to be working. I managed to determine the right instance to use in JS to call send a transaction object to my contract, and the contract correctly splits the ether between two accounts.
    Next steps:
    --Remove hard coded accounts, which allows for more flexible testing
    --Update test cases so the truffle test will work
    --Experiment with Promises to improve execution and readability

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -30,7 +30,9 @@ function refreshBalance() {
         .then(
             function(Alice) {
                 acctAlice = Alice;
-                console.log('acctAlice', acctAlice);
+                // This web3 instance returns a callback
+                // Perhaps that is why the Dapp loads slowly sometimes
+                // I wonder if changing it to a Promise would help? TODO
                 web3.eth.getBalance(acctAlice, 
                     function(err, balAlice) {
                         if (err != null) {
@@ -47,7 +49,6 @@ function refreshBalance() {
     split.Bob()
         .then(function(Bob) {
             acctBob = Bob;
-            console.log('acctBob', acctBob);
             web3.eth.getBalance(acctBob, function(err, balBob) {
                 if (err != null) {
                     alert("There was an error fetching your accounts.");
@@ -61,7 +62,6 @@ function refreshBalance() {
     split.Carol()
         .then(function(Carol) {
             acctCarol = Carol;
-            console.log('acctCarol', acctCarol);
             web3.eth.getBalance(acctCarol, function(err, balCarol) {
                 if (err != null) {
                     alert("There was an error fetching your accounts.");
@@ -71,15 +71,6 @@ function refreshBalance() {
                 balCarol_element.innerHTML = web3.fromWei(balCarol, 'ether');
             });
         });
-
-//        split.getBalance.call(account, {from: account}).then(function(value) {
-//    var balance_element = document.getElementById("balance");
-//    balance_element.innerHTML = value.valueOf();
-//  }).catch(function(e) {
-//    console.log(e);
-//    setStatus("Error getting balance; see log.");
-//  });
-
 };
 
 function sendCoin() {
@@ -91,29 +82,25 @@ function sendCoin() {
 
     setStatus("Initiating transaction to send " + amount + " wei ... (please wait)");
 
-    //web3.eth.sendTransaction({from:acctOwner, to:acctSplit, value:amount}, function(err, txHash) {
     // We need to interact with the web3 layer to allow the user to authorize the transaction.
-    split.contract.sendSplit.sendTransaction({from:acctOwner, to:acctSplit, value:amount}, 
-            function( err, txHash ) {
-                if (err != null) {
-                    setStatus("There was an error sending ether to Split contract.");
-                    return;
-                }
-                else {
-                    setStatus("Waiting for transaction to be mined...");
-                    // sendTransaction should return the transaction hash
-                    // if this Promise works, then the UI will be updated.
-                    // Metamask doesn't like this, complains it's synchronous...
-                    // Is that because of the 'contract' instance above?
-                    // Taking out the 'contract' results in invalid address, so I am stuck.
-                    return web3.eth.getTransactionReceiptMined(txHash)
-                        .then( function(receipt) {
-                            setStatus("Transaction complete.");
-                            refreshBalance();
-                        });
-                }
+    // This should be web3 instance with callback
+    split.contract.sendSplit({from:acctOwner, to:acctSplit, value:amount}, 
+        function( err, txHash ) {
+            if (err != null) {
+                setStatus("There was an error sending ether to Split contract.");
+                return;
             }
-        );
+            else {
+                setStatus("Waiting for transaction to be mined...");
+                web3.eth.getTransactionReceipt(txHash, 
+                    function(receipt) {
+                        setStatus("Transaction complete.");
+                        refreshBalance();
+                    }
+                );
+            }
+        }
+    );
 };
 
 window.onload = function() {
@@ -135,43 +122,8 @@ window.onload = function() {
         accounts = accs;
         // This is not necessarily the contract address!!
         acctOwner = accounts[0];
-        console.log('accounts', accounts);
 
         // Get the balances for the other accounts registered in the contract
         refreshBalance();
     });
-
-    // Maybe this function should be defined elsewhere, why call it onload?
-    web3.eth.getTransactionReceiptMined = function (txnHash, interval) {
-        var transactionReceiptAsync;
-        interval = interval ? interval : 500;
-        transactionReceiptAsync = function(txnHash, resovle, reject) {
-            try {
-                var receipt = web3.eth.getTransactionReceipt(txnHash);
-                if (receipt == null) {
-                    setTimeout(function () {
-                                transactionReceiptAsync(txnHash, resolve, reject); },  
-                                interval);
-                }
-                else {
-                    resolve(receipt);
-                }
-            } catch(e) {
-                reject(e);
-            }
-        };
-
-        if (Array.isArray(txnHash)) {
-            var promises = [];
-            txnHash.forEach(function (oneTxHash) {
-                promises.push(web3.eth.getTransactionReceiptMined(oneTxHash, interval));
-            });
-            return Promise.all(promises);
-        }
-        else {
-            return new Promise(function (resolve, reject) {
-                transactionReceiptAsync(txnHash, resolve, reject);
-            });
-        }
-    };
 }
